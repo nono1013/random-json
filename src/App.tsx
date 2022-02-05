d24c2e6a122405abb369770c08586954a6446f13
nono1013 <gnono1013@gmail.com>
2022-02-05 18:40:56 +0100

    Dummy json and linter setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
+/* eslint-disable react/no-array-index-key */
+
 import React, { useState } from 'react';
+import dummyJson from './dummy.json';
+
+type Json = Record<string, unknown>;
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [dummy] = useState(dummyJson);
+	const isArray = (obj: unknown) => {
+		return Object.prototype.toString.call(obj) === '[object Array]';
+	};
 
 	return (
 		<div>
-			<header>
-				<p>
-					<button
-						className="text-red-400"
-						type="button"
-						onClick={() => setCount((count) => count + 1)}
-					>
-						count is: {count}
-					</button>
-				</p>
-			</header>
+			<main className="p-4">
+				{dummy.map((data: Json, i: number) => {
+					return (
+						<div key={`data-${i}`}>
+							{Object.keys(data).map((e: string) =>
+								(isArray(data[e]) || (
+									<p key={`dummy-${e.toString()}`}>
+										{e}
+										{': '}
+										{data[e]}
+									</p>
+								)))}
+						</div>
+					);
+				})}
+			</main>
 		</div>
 	);
 }
